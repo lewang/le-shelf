6eb141c67cf93e0d882da332764d46789ad966cc
Le Wang <lewang@todaq-le-mba.local>
2025-11-04 11:55:30 -0500

    make le-forward-sentence-smart know backward

--- le-shelf.el
+++ le-shelf.el
@@ -30,7 +30,6 @@
 
 
 
-;;;###autoload
 (defun le-is-text (&optional pos)
   "Return t if POS should be treated as text.
 
@@ -42,17 +41,6 @@ That is comment or string in programming modes or anywhere in text mode."
     (or (not comment-start)
         (syntax-ppss-context (syntax-ppss)))))
 
-(defun le-beginning-of-sexp-scope (arg)
-  "Go to beginning of current sexp's scope.  If point doesn't
-move then go backward up one sexp level."
-  (if (le-is-text)
-      (forward-sentence-default-function (* -1 arg))
-    (let ((orig-point (point)))
-      (paredit-backward-up arg)
-      (unless (= (+ (point) 1) orig-point)
-        (forward-char))
-      (point))))
-
 ;;;###autoload
 (defun le-forward-sentence-smart (arg)
   "Go to end of sexp.  Invoke again to move out of sexp.
@@ -61,26 +49,27 @@ When inside strings, move by sentence instead.
 
 Approprfiate for use as `forward-sentence-function'"
   (or arg (setq arg 1))
-  (if (< arg 0)
-      (le-beginning-of-sexp-scope (* -1 arg))
-    (let ((list
-           ;; Get list of positions end of current context and before ")", or
-           ;; end of sentence choose next one.
-           `(,@(save-excursion
-                 (ignore-errors
-                   (paredit-forward-up arg)
-                   (list (1- (point))
-                         (point))))
-             ,@(when (le-is-text)
-                 (save-excursion
-                   (forward-sentence-default-function arg)
-                   (list (point)))))))
-      (when list
-        (goto-char (apply 'min
-                          ;; second call must move.
-                          (if (eq last-command this-command)
-                              (delq (point) list)
-                            list)))))))
+  (let ((list
+         ;; Get list of positions end of current context and before ")", or
+         ;; end of sentence choose next one.
+         `(,@(save-excursion
+               (ignore-errors
+                 (paredit-forward-up arg)
+                 (list (- (point) (if (< arg 0)
+                                      -1 1))
+                       (point))))
+           ,@(when (le-is-text)
+               (save-excursion
+                 (forward-sentence-default-function arg)
+                 (list (point)))))))
+    (when list
+      (goto-char (apply (if (< arg 0)
+                            'max
+                          'min)
+                        ;; second call must move.
+                        (if (eq last-command this-command)
+                            (delq (point) list)
+                          list))))))
 
 
 (provide 'le-shelf)
